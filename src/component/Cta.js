4a6dcf9c3eb1d1cb673e101c417be18ab78b0d66
oseghalee <osespecial1@gmail.com>
2023-11-23 15:20:25 +0100

    update

--- src/component/Cta.js
+++ src/component/Cta.js
@@ -1,20 +1,19 @@
 import React from 'react'
 import {FaFacebookF, FaInstagram, FaWhatsapp, } from 'react-icons/fa'
 import {BiLogoGmail} from 'react-icons/bi'
-import { Link } from 'react-router-dom'
 
 const Cta = () => {
   return (
     <div className='px-6 pt-8' id='Cta'>
       <div className='container mx-auto flex justify-center items-center pb-[5rem] md:pt-4 md:p-[5rem] w-full '>
-        <div className='bg- items-center rounded-lg shadow-xl shadow-white p-4 py-7 md:p-8 md:px-16 md:flex md:gap-10 md:justify-start'>
+        <div className='items-center rounded-lg shadow-xl shadow-white p-4 py-7 md:p-8 md:px-16 md:flex md:gap-10 md:justify-start'>
           <div className=''>
             <h2 className='py-2 px-6 text-2xl font-semibold  text-purple-700 md:text-4xl md:max-w-sm md:text-left md:font-bold '>Bring to realization  your ideas...</h2>
           </div>
           <div>
-            <h1 className='text-xl md:text-2xl font-bold text-center leading-6 tracking-wider text-black '>Contact</h1>
-            <h3 className=' py-2 text-lg text-gray-700 text-center md:py-4'>Call : +234 902 6898 778</h3>
-            <div className='flex justify-center items-center gap-4'>
+            <h1 className='text-xl pt-2 md:text-2xl font-bold text-center leading-6 tracking-wider text-black '>Contact</h1>
+            <h3 className=' py-2 text-lg text-gray-700 text-center'>Call : +234 902 6898 778</h3>
+            <div className='flex justify-center items-center gap-4 pt-2'>
               <a href="https://www.facebook.com/oseghale.osemudiamwen">
                 <div className='p-2 bg-purple-100 text-purple-900 rounded-md transition duration-300 ease-in-out hover:scale-110 ' ><FaFacebookF size = {20} /> </div>
               </a>
